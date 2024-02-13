837b3a49d37a89ea653e1b816622faabc0538977
Abu Hurrara <abuhurrara1093@gmail.com>
2024-02-13 11:46:10 +0500

    add CRUD functions

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,13 @@
 package controllers
+
+import(
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"bookstore/pkg/utils"
+	"bookstore/pkg/models"
+)
+
+var NewBook models.Book
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -7,7 +7,7 @@ import (
 
 var db *gorm.DB
 
-type book struct {
+type Book struct {
 	gorm.Model
 	Name         string `gorm:"" json:"name"`
 	Author       string `json:"author"`
@@ -17,10 +17,10 @@ type book struct {
 func init() {
 	config.Connect()
 	db = config.GetDB()
-	db.AutoMigrate(&book{})
+	db.AutoMigrate(&Book{})
 }
 
-func (b *book) CreateBook() *book {
+func (b *Book) CreateBook() *Book {
 	db.NewRecord(b)
 	db.Create(&b)
 	return b
@@ -30,4 +30,16 @@ func GetAllBooks() []Book{
 	var Books []Book
 	db.Find(&Books)
 	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
 }
